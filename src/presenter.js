1416c0f9fbae639fde70dbac8b69e5a3aad5cf5a
Cristian Mendoza <cristian.mendoza.h1@gmail.com>
2025-02-13 10:31:51 -0400

    se corrigio y se aniadio definitivamente el saludar por hora

--- src/presenter.js
+++ src/presenter.js
@@ -7,8 +7,9 @@ const resultDiv = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const nombre = nameInput.value.trim();
+  
   if (nombre) {
-    resultDiv.innerHTML = <p>${saludar(nombre)}</p>;
+    resultDiv.innerHTML = `<p>${saludar(nombre)}</p>`;
   } else {
     resultDiv.innerHTML = "<p>Por favor, ingresa tu nombre.</p>";
   }
--- src/saludar.test.js
+++ src/saludar.test.js
@@ -3,5 +3,5 @@ import saludar from "./saludar.js";
 describe("Saludar", () => {
   it("debería saludar con el nombre y el saludo adecuado", () => {
     expect(saludar("Nicole")).toMatch(/Nicole, bienvenido!/);
-  });
+ });
 });
